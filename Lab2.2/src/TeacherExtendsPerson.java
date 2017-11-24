5237af9f1987255c225a525a6f3926753abb42c2
handy <handy@DESKTOP-TMT5556>
2017-11-23 21:46:54 -0500

    Update

--- Lab2.2/src/TeacherExtendsPerson.java
+++ Lab2.2/src/TeacherExtendsPerson.java
@@ -1,7 +1,23 @@
 
 public class Teacher extends Person {
+	private String Subject;
+	private String Title;
+	
 	public Teacher(String firstName, String familyName, int height, String zodiacSign, String Subject, String Title) {
 		super(firstName, familyName, height, zodiacSign);
+		this.Subject = Subject;
+		this.Title = Title;
 	}
 
+	public String getSubject() {
+		return Subject; 
+	}
+	
+	public String getTitle() {
+		return Title;
+	}
+	
+	public String toString() {
+		return("Teacher = " + Title + super.getFamilyName());
+	}
 }
