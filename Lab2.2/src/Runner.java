05078090315b414a4007280cdcd459c7d780e255
jlau9471 <32362437+jlau9471@users.noreply.github.com>
2017-11-28 09:27:26 -0500

    Update Runner.java

--- Lab2.2/src/Runner.java
+++ Lab2.2/src/Runner.java
@@ -1,7 +1,7 @@
 
 public class Runner {
 	public static void main(String[] args) {
-		String[] firstNames = {"Annie", "Jessica", "Nicholas", "Kevin", "Daniel", "Ash", "Jane", "Robbie", "Carlos", "Doyle", "Randall", "Jaiden", "William", "Samuel"};
+		String[] firstNames = {"Annie", "Jessica", "Nicholas", "Kevin", "Daniel", "Ash", "Jane", "Robbie", "Carlos", "Doyle", "Randall", "Jaiden", "William", "Samuel", "Steven"};
 		String[] familyNames = {"Wu", "Chang", "Ng", "Mash", "Lin", "Feliz", "Johnson", "Jones", "Wood", "Boyd", "Li", "Chen", "Moy", "Chan", "Huang"};
 	}
 }
